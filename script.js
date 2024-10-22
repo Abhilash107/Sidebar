248998205e8eabf01985b9832a96ef758ad46732
Olajuwon47 <lawal.olajuwonwaheed@gmail.com>
2024-10-22 12:25:08 +0100

    Add code to line 63

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var spinner = document.querySelector('#mySpinner');
-    if (spinner) {
-      spinner.addEventListener('mdl-componentupgraded', function() {
-        this.MaterialSpinner.setSpinner(44);
-      });
+// Back to Top button functionality
+let backToTopBtn = document.getElementById("backToTopBtn");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
     }
-  });
+};
+
+backToTopBtn.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
   
 
   
